40b5369b027d4a624dd7c3138cd9a679922de719
golova-m <golova.m@bk.ru>
2024-10-22 11:59:30 +0400

    Add structure login

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get('/start')
-def get_start():
-    return {'data': 'Hello, world!'}
+@app.get('/')
+def start(request: Request):
+    return templates.TemplateResponse('start.html', {'request': request})
+
+
+@app.get('/login', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse('login.html', {'request': request})
+
+
+@app.post("/postdata")
+def postdata(request: Request):
+    return templates.TemplateResponse('start.html', {'request': request})
